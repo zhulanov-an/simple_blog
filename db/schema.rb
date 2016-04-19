88df4fa88eaad09f424a9c25b580a4be59c32b8c
Zhulanov Anton <zhulanov-an@dartit.ru>
2016-04-19 17:12:00 +0500

    add model Contact

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160418112257) do
+ActiveRecord::Schema.define(version: 20160419120947) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "email"
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
