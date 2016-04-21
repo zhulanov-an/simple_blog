1ecd80c0a9ba255d5656e1d8ccbadebc1cad01e3
Zhulanov Anton <zhulanov-an@dartit.ru>
2016-04-21 19:27:10 +0500

    remove example posts from routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :articles, only: [:new, :create, :index, :edit, :update, :destroy]
   get 'about' => 'home#about'
   get 'terms' => 'home#terms'
-  resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
