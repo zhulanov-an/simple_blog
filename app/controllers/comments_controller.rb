d90b75c2c6b80de736cd013ceb560603fa583eb4
Zhulanov Anton <zhulanoff2007@gmail.com>
2016-04-22 00:23:56 +0500

    add comments to post db

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,16 @@
 class CommentsController < ApplicationController
   def create
-    get_article.comments.create({author: 'Jogn', body: '906090'})
+    get_article.comments.create(get_params)
+    redirect_to article_path(get_article)
   end
 
   private
-    def get_article
+
+  def get_params
+    params.require(:comment).permit(:author, :body)
+  end
+
+  def get_article
       Article.find(params[:article_id])
-    end
+  end
 end
