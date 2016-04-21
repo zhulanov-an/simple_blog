4e68847f895d6d85d23571c0e5b63d80a996ba4a
Zhulanov Anton <zhulanov-an@dartit.ru>
2016-04-21 20:10:46 +0500

    add secure get params and example create comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,10 @@
 class CommentsController < ApplicationController
+  def create
+    get_article.comments.create({author: 'Jogn', body: '906090'})
+  end
+
+  private
+    def get_article
+      Article.find(params[:article_id])
+    end
 end
