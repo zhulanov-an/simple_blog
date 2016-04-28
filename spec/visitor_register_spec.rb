6b186a9fbaf6e928107e558a5940c60654d5c003
Zhulanov Anton <zhulanov-an@dartit.ru>
2016-04-28 18:37:55 +0500

    add support to sign up page

--- spec/visitor_register_spec.rb
+++ spec/visitor_register_spec.rb
@@ -3,6 +3,6 @@ require "spec_helper"
 feature "Registration visitors" do
   scenario "allows access to register page" do
     visit "/users/sign_up"
-    expect(page).to have_content "Sign up page"
+    expect(page).to have_content I18n.t("registration.sign_up")
   end
 end
