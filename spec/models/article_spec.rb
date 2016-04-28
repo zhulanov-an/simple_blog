dc5bd80ee1aacd13ab5bacde00e2f8d1a96de7bc
Zhulanov Anton <zhulanov-an@dartit.ru>
2016-04-28 14:40:19 +0500

    improve article test

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,7 +1,14 @@
 require "spec_helper"
 
 describe Article do
-  it {should validate_presence_of(:title)}
-  it {should validate_presence_of(:text)}
-  it {should have_many :comments}
+
+  describe "validations" do
+    it {should validate_presence_of(:title)}
+    it {should validate_presence_of(:text)}
+  end
+
+  describe "relations" do
+    it {should have_many :comments}
+  end
+  
 end
