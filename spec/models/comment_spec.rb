3202acd85d1c09cfff53e6f4500998019f84bc97
Zhulanov Anton <zhulanov-an@dartit.ru>
2016-04-28 17:31:23 +0500

    add test length text field comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,4 +4,9 @@ describe Comment do
   describe "relations" do
     it {should belong_to :article}
   end
+  describe "validations" do
+    it "validate maximum char to body" do
+      should validate_length_of(:body).is_at_most(4000).with_long_message('is too long (maximum is 4000 characters)')
+    end
+  end
 end
